7f1e2851f9e0ef94a634164dba17ed5840c0092a
Jhonny <jh.hueller@gmail.com>
2021-06-16 20:21:41 +0200

    Updated Ngrammer

--- ngrammer/Ngrammer.py
+++ ngrammer/Ngrammer.py
@@ -1,3 +1,6 @@
+from math import log
+
+
 def log2prob(log_value):
     from math import exp
     return exp(log_value) if log_value else 0.0
@@ -5,8 +8,8 @@ def log2prob(log_value):
 
 def perplexity(tree, sentence):
     n = len(sentence)
-    probability = tree.get_word(sentence).probability
-    perp = (1/probability)**(1/n)
+    probability = tree.score(sentence)
+    perp = (1 / probability) ** (1 / n)
     return perp
 
 
@@ -20,6 +23,12 @@ class Node:
         self.count = 0
         self.children = dict()
 
+    def __set__(self, instance, value):
+        self.instance = value
+
+    def __get__(self, instance, owner):
+        return self.instance
+
     def __str__(self):
         return self.string
 
@@ -31,9 +40,9 @@ class PrefixTree:
 
     def __init__(self, n=None):
         self.root = Node(".")
-        self.error = Node()
+        self.error = Node("Error")
         self.n = n
-        self.probabilities = False
+        self.interpolation = None
 
     def add_ngram(self, sequence):
         node = self.root
@@ -43,13 +52,15 @@ class PrefixTree:
             node = node.children[word]
             node.count += 1
 
-    def get_word(self, sequence):
+    def get(self, sequence):
         node = self.root
+        sequence = sequence[-self.n:]
         for word in sequence:
             node = node.children.get(word, self.error)
         return node
 
     def traverse(self, node=None, sequence=None, size=None):
+
         sequence = sequence if sequence else []
         node = self.root if not node else node
 
@@ -65,16 +76,36 @@ class PrefixTree:
             yield from self.traverse(n, sequence, size)
             sequence.pop()
 
-    def vocabulary(self, n=1):
+    def vocabulary(self, n=None):
+        n = self.n if n is None else n
         v = set()
         for ngram in self.traverse():
             v.add("_".join(ngram[:n]))
         v = sorted(list(v))
         return v
 
-    def predict_ngram(self, sequence):
-        probability = self.get_word(sequence[-self.n:]).probability
-        return probability
+    def score(self, sentence):
+        from math import prod
+        probabilities = list()
+        for ngram in PrefixTree.extract_ngrams(sentence, self.n):
+            probabilities.append(self.get(ngram).probability)
+        return sum(probabilities) if self.logs else prod(probabilities)
+
+    def __deleted_interpolation__(self):
+        w = [0] * self.n
+        for ngram in self.traverse():
+            # current ngram count
+            v = self.get(ngram).count
+            # (n)-gram counts
+            n = [self.get(ngram[0:i + 1]).count for i in range(len(ngram))]
+            # (n-1)-gram counts -- parent node
+            p = [self.get(ngram[0:i]).count for i in range(len(ngram))]
+            # -1 from both counts & normalize
+            d = [float((n[i] - 1) / (p[i] - 1)) if (p[i] - 1 > 0) else 0.0 for i in range(len(n))]
+            # increment weight of the max by raw ngram count
+            k = d.index(max(d))
+            w[k] += v
+        self.interpolation = [float(v) / sum(w) for v in w]
 
     @staticmethod
     def extract_ngrams(sequence, n):
@@ -87,32 +118,36 @@ class PrefixTree:
     @staticmethod
     def store_ngrams(corpus, n, tree=None):
         assert n > 0, "n must be greater than 0, given: {}".format(n)
-        tree = PrefixTree() if not tree else tree
-        tree.n = n
+        tree = PrefixTree(n) if not tree else tree
+
         for sequence in corpus:
-            for ngram in PrefixTree.extract_ngrams(sequence, n):
+            for ngram in PrefixTree.extract_ngrams(sequence, tree.n):
                 tree.add_ngram(ngram)
         return tree
 
     @staticmethod
     def compute_probabilities(tree, logs=False, smoothing=False):
-        from math import log
         tree.logs = logs
         tree.smoothing = smoothing
 
-        v = len(tree.vocabulary(tree.n)) if tree.smoothing else 0
+        v = len(tree.vocabulary(tree.n - 1)) if tree.smoothing else 0
         a = 1 if tree.smoothing else 0
 
         tree.error.probability = log(a / v) if (smoothing and logs) else 0.0
 
         for ngram in tree.traverse():
-            n = tree.get_word(ngram)
-            p = tree.get_word(ngram[:-1])
+            n = tree.get(ngram)
+            p = tree.get(ngram[:-1])
             prob = (n.count + a) / (p.count + v)
             n.probability = log(prob) if logs else prob
 
-        tree.probabilities = True
         return tree
+
+    def __set__(self, instance, value):
+        self.instance = value
+
+    def __get__(self, instance, owner):
+        return self.instance
 
 
 class MultiNgramPrefixTree:
@@ -120,6 +155,7 @@ class MultiNgramPrefixTree:
     A class used almost only to encapsulate the use of multiple prefix trees
     used to smooth out prediction
     """
+
     def __init__(self, n):
         assert n > 0, "n must be greater than 0, given: {}".format(n)
         self.n = n
@@ -133,52 +169,63 @@ class MultiNgramPrefixTree:
         n = len(sequence) if n is None else n
         self.trees[n].add_ngram(sequence)
 
-    def get_word(self, sequence, n=None):
-        n = len(sequence) if n is None else n
-        self.trees[n].get_word(sequence)
+    def get(self, sequence, n=None):
+        n = self.n if n is None else n
+        self.trees[n].get(sequence)
 
     def traverse(self, n=None, node=None, sequence=None, size=None):
         n = len(self.trees) if n is None else n
         self.trees[n].traverse(node, sequence, size)
 
     def vocabulary(self, n=None):
-        n = 1 if n is None else n
-        v = set()
-        for ngram in self.trees[n].traverse():
-            v.add("_".join(ngram[:n]))
-        v = sorted(list(v))
-        return v
+        n = self.n if n is None else n
+        return self.trees[n].vocabulary(n)
 
-    def predict_ngram(self, sequence):
-        if self.coefficients is None:
-            print("There are no coefficients for interpolation, reverting to greater n prediction")
-            n = min(len(sequence), self.n)
-            return self.trees[n].predict_ngram(sequence)
+    def predict_sentence(self, sentence, n=None, use_interpolation=False):
+        n = self.n if n is None else n
+        if not use_interpolation:
+            return self.trees[n].score(sentence)
 
-        total_coefficients = sum([value for n, value in self.coefficients.items()])
-        assert total_coefficients > 1, "The sum of the coefficient is greater than 1"
+        if not self.trees[n].interpolation:
+            self.trees[n].__deleted_interpolation__()
 
-        if len(self.coefficients) != len(self.trees):
-            print("The number of coefficients ({}) and trees ({}) do not match".format(len(self.coefficients), len(self.trees)))
+        from math import prod
+        coefficients = self.trees[n].interpolation
+        probabilities = list()
 
-        probability = 0.
-        for i in range(1, len(self.trees) + 1):
-            probability += self.coefficients[i] * self.trees[i].predict_ngram(sequence)
+        for ngram in PrefixTree.extract_ngrams(sentence, n):
+            multigram_probabilities = list()
 
-        return probability
+            for i in range(1, n + 1):
+                node = self.trees[i].get(ngram)
+                multigram_probabilities.append(node.probability)
+            for i in range(len(multigram_probabilities)):
+                multigram_probabilities[i] *= coefficients[i]
+
+            interpolated_ngram_probability = sum(multigram_probabilities)
+
+            probabilities.append(interpolated_ngram_probability)
+        return sum(probabilities) if self.logs else prod(probabilities)
+
+    def set_interpolations(self):
+        for n, tree in self.trees.items():
+            tree.__deleted_interpolation__()
 
     @staticmethod
     def store_ngrams(corpus, n, tree=None):
         assert n > 0, "n must be greater than 0, given: {}".format(n)
         tree = MultiNgramPrefixTree(n) if not tree else tree
         for sequence in corpus:
-            for i in range(1, n+1):
+            for i in range(1, n + 1):
                 for ngram in PrefixTree.extract_ngrams(sequence, i):
                     tree.add_ngram(ngram)
         return tree
 
     @staticmethod
     def compute_probabilities(multi_tree, logs=False, smoothing=False):
+        multi_tree.logs = logs
+        multi_tree.smoothing = smoothing
+
         for n, tree in multi_tree.trees.items():
             PrefixTree.compute_probabilities(tree, logs, smoothing)
         return multi_tree
