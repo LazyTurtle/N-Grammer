943541a66a8ccca8bbd82018c8fb193a7d267acb
Jhonny <jh.hueller@gmail.com>
2021-06-22 15:20:40 +0200

    Start testing

--- main.py
+++ main.py
@@ -47,17 +47,22 @@ def extract_coefficients(n, corpus):
 
 
 def test_trees(trees, test_data):
+    print("Start testing")
+    print_progress_bar(0, n_samples, prefix='Progress:', suffix='Complete', length=50)
     n_test_data = len(test_data)
     perplexities = defaultdict(int)
-    for sentence in test_data:
+    for i in range(n_test_data):
+
         for name, tree in trees.items():
-            perplexities[tree]+=calc_perplexity(tree,sentence)
+            perplexities[tree] += calc_perplexity(tree, test_data[i])
+        print_progress_bar(i + 1, n_samples, prefix='Progress:', suffix='Complete', length=50)
+
 
     for tree, perplexity in perplexities.items():
-        perplexities[tree]/=n_test_data
+        perplexities[tree] /= n_test_data
 
     for name, tree in trees.items():
-        print("Mean Perplexity {}: {}".format(name,perplexities[tree]))
+        print("Mean Perplexity {}: {}".format(name, perplexities[tree]))
 
 
 # Press the green button in the gutter to run the script.
@@ -74,17 +79,25 @@ if __name__ == '__main__':
     test_corpus = CorpusHandler.load_corpus(test, annotations)
 
     N = 3
-
+    print("Calculating interpolation coefficients")
     coefficients = extract_coefficients(N, validation_corpus)
+    turing = CorpusHandler.good_turing_estimate(test_corpus)
 
     print("Instantiating trees")
     trees = dict()
     trees["Standard prefix tree"] = PrefixTree(N)
-    trees["Single cache(200) prefix tree"] = CachedPrefixTree(N)
+    trees["Single cache(400) prefix tree"] = CachedPrefixTree(N, 400)
+    trees["Single cache(200) prefix tree"] = CachedPrefixTree(N, 200)
     trees["Single cache(100) prefix tree"] = CachedPrefixTree(N, 100)
     trees["Single cache(50) prefix tree"] = CachedPrefixTree(N, 50)
+    trees["Single cache(25) prefix tree"] = CachedPrefixTree(N, 25)
+    trees["Multi ngram cached(400) prefix tree"] = CachedMultiNgramPrefixTree(N, 400)
+    trees["Multi ngram cached(200) prefix tree"] = CachedMultiNgramPrefixTree(N, 200)
+    trees["Multi ngram cached(100) prefix tree"] = CachedMultiNgramPrefixTree(N, 100)
+    trees["Multi ngram cached(50) prefix tree"] = CachedMultiNgramPrefixTree(N, 50)
+    trees["Multi ngram cached(25) prefix tree"] = CachedMultiNgramPrefixTree(N, 25)
     trees["Multi cache(200,100,50) prefix tree"] = CachedPrefixTree(N, [200, 100, 50])
-    trees["POS prefix tree"] = PosTree(N)
+    trees["Multi ngram cached(200,100,50) prefix tree"] = CachedMultiNgramPrefixTree(N, [200, 100, 50])
 
     print("Loading train data")
     print("")
@@ -92,8 +105,9 @@ if __name__ == '__main__':
     n_samples = len(train_corpus)
 
     """
-    The pos tree takes an enormous amount of time to load
+    The pos tree takes a lot of time to store all the data, even more than 5 minutes
     this is because it uses spacy to parse every single sentence one by one singularly
+    The batch loading doesn't seem to work properly on my machine
     
     In the case one uses pycharm to run this program, 
     it is necessary to enable the terminal simulation for the debug console
@@ -105,16 +119,18 @@ if __name__ == '__main__':
         for i in range(n_samples):
             tree.store(train_corpus[i])
             print_progress_bar(i + 1, n_samples, prefix='Progress:', suffix='Complete', length=50)
+        tree.construct_vocabulary(train_corpus)
         print("")
 
-    logs = False
-    smoothing = False
+    logs = True
+    smoothing = True
     for name, tree in trees.items():
         print("Training: ", name)
         tree.train(logs, smoothing)
+        tree.turing = turing
 
     for name, tree in trees.items():
         if hasattr(tree.__class__, 'set_coefficients') and callable(getattr(tree.__class__, 'set_coefficients')):
             tree.set_coefficients(coefficients)
 
-    test_trees(trees,test_corpus)
+    test_trees(trees, test_corpus)
